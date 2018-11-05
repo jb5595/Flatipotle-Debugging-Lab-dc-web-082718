cfb5167ffb0ff6b539dad92b5bf5395e302a0d24
jb5595 <jacobberman1995@gmail.com>
2018-11-05 15:20:48 -0500

    Done.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Side from "./Side"
 
 class Order extends Component {
   state = {
@@ -6,10 +7,14 @@ class Order extends Component {
   }
 
   handleClick = () => {
-    this.state.isClicked = !this.state.isClicked
+    this.setState({
+      isClicked: !this.state.isClicked
+    })
   }
 
   render() {
+    let sides = <Side sides={this.props.sides} />
+    console.log(sides)
     return (
       <div className="ui centered raised card">
         <div className="image">
@@ -37,6 +42,7 @@ class Order extends Component {
 
           { /* this is just a shortcut to writing this.state.isClicked ? <Side sides={this.props.sides} /> : null */ }
           { this.state.isClicked && <Side sides={this.props.sides} /> }
+
 
         </div>
       </div>
